24b34e2bfe794c0f3b153d8b3682f285a1e76c73
anabar3 <ana.barrera@udc.es>
2025-03-27 15:58:18 +0100

    add app.py

---
+++ src/app/app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
